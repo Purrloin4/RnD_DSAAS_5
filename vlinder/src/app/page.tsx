715905bd45236c7202b736d6a26072872e2927f5
eltajj18 <100543589+eltajj18@users.noreply.github.com>
2024-11-10 15:18:40 +0100

    Revert "47 create login page"

--- vlinder/src/app/page.tsx
+++ vlinder/src/app/page.tsx
@@ -1,18 +1,5 @@
 import Hello from 'Components/hello';
-import {logout} from "./logout/actions";
-import React from "react";
+
 export default function Page() {
-
-
-
-  return (
-
-
-   <Hello />
-// <form action={logout}>
-//     <button type="submit">
-//      Logout
-//     </button>
-//     </form >
-    )
+    return <Hello />
   }
