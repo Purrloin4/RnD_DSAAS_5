fc3e4d61d76a99594ffafca7c12bf19e0cf67f35
Eltaj Amirli <amirli.eltaj18@gmail.com>
2024-11-14 20:20:42 +0100

    made some progress on chatting

---
+++ vlinder/src/app/direct_message/page.tsx
@@ -0,0 +1,63 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import { createClient } from '@/utils/supabase/server'
+import { Input } from '@supabase/ui'
+import Messages from '@/src/components/messages'
+
+
+const supabase = createClient()
+
+const Home: NextPage = () => {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const form = e.currentTarget
+        const { message } = Object.fromEntries(new FormData(form))
+        if (typeof message === 'string' && message.trim().length !== 0) {
+            form.reset()
+            const { error } = await supabase
+                .from('messages')
+                .insert({ content: message })
+            if (error) {
+                alert(error.message)
+            }
+        }
+    }
+
+    const handleCreateRoom = async () => {
+        // await supabase.from('rooms').insert({}, { returning: 'minimal' })
+
+        await supabase.from('rooms').insert([{}, { returning: 'minimal' }])
+        const { data } = await supabase
+            .from('rooms')
+            .select('*')
+            .order('created_at', { ascending: false })
+            .limit(1)
+            .single()
+        console.log({ data })
+    }
+
+
+
+
+    return (
+        <div className="flex h-screen flex-col items-center justify-center">
+            <Head> <title>Happy Chat</title>
+                <link rel="icon" href="/favicon.ico" />
+
+            </Head>
+
+            <main className="flex h-full w-full flex-1 flex-col items-stretch bg-blue-400 py-10 px-20 text-gray-800">
+                <h1 className="bg-green-200 px-4 py-2 text-4xl">Happy Chat  <button className="ml-4 rounder border bg-red-200 p-2 text-xs"
+                    onClick={handleCreateRoom}>
+                    New Room</button>
+                </h1>
+                <Messages />
+                <form onSubmit={handleSubmit} className="bg-red-200 p-2">
+                    <Input type="text" name="message" />
+                </form>
+            </main>
+        </div>
+    )
+}
+
+export default Home
---
+++ vlinder/src/components/messages.tsx
@@ -0,0 +1,87 @@
+'use client'
+
+import { useEffect, useState, useRef } from 'react'
+import supabase from '@/utils/supabase/supabase'
+
+type Message = {
+  id: string
+  created_at: string
+  content: string
+  profile_id: string
+  profile: {
+    username: string
+  }
+}
+export default function Messages() {
+  const [messages, setMessages] = useState<Message[]>([])
+  const messagesRef = useRef<HTMLDivElement>(null)
+    const getData = async () => {
+    const { data } = await supabase
+      // .from<Message>('messages')
+      .from('messages')
+      .select('*, profile: profiles(username)')
+
+    if (!data) {
+      alert('no data')
+      return
+    }
+    setMessages(data)
+    if(messagesRef.current){
+      messagesRef.current.scrollTop = messagesRef.current.scrollHeight
+    }
+  }
+  useEffect(() => {
+    getData()
+  }, [])
+  useEffect(() => {
+    const subscription = supabase
+    .from<Message>('messages')
+    .on('INSERT', () => {
+      getData()
+    })
+    .subscribe()
+      // .channel('messages')
+      // .on(
+      //   'postgres_changes', // Event type for listening to database changes
+      //   {
+      //     event: 'INSERT',
+      //     schema: 'public',
+      //     table: 'messages',
+      //   },
+      //   () => {
+      //     getData(); // Fetch new data on each new insert
+      //   }
+      // )
+      // .subscribe();
+  
+  
+    return () => {
+      // supabase.removeSubscription(subscription)
+      supabase.removeChannel(subscription)
+    }
+  }, [])
+
+  const userId = supabase.auth.user()?.id
+  console.log({ messages })
+  return (
+    <div className="flex-1 overflow-y-scroll bg-pink-200"ref={messagesRef}>
+      <ul className="flex flex-col justify-end space-y-1 p-4">
+        {messages.map((message) => (
+          <li
+            key={message.id}
+            className={
+              message.profile_id === userId
+                ? 'self-end rounded bg-blue-400 px-2'
+                : 'self-start rounded bg-gray-100 px-2'
+            }
+          >
+            <span className="block text-xs text-gray-500">
+              {message.profile.username}
+            </span>
+            <span className="">{message.content}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
