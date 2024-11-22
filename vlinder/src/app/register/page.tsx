0d354a7d079b45b395d77fc631f2d1978bd79823
Thiewout Bekaert <thiewout@gmail.com>
2024-11-22 14:35:45 +0100

    tussen commit

--- vlinder/src/app/register/page.tsx
+++ vlinder/src/app/register/page.tsx
@@ -1,35 +1,34 @@
-"use client"
-import { redirect, useRouter } from 'next/navigation'
-import { useState, useEffect, useMemo } from 'react';
+"use client";
+import { redirect, useRouter } from "next/navigation";
+import Link from "next/link"
+import { useState, useEffect, useMemo } from "react";
 
 //Next-ui components
-import { Input, Link, Button } from "@nextui-org/react";
-
+import { Input, Button } from "@nextui-org/react";
 
 //Components
-import Logo from 'Components/Logo'
+import Logo from "Components/Logo";
 
 function Page() {
-    return (
-        <main className="flex items-center justify-center min-h-screen px-4"> 
-  <div className="w-full max-w-md p-8 h-fit bg-white rounded-md shadow-md">
-    <Logo alt="logo vlinder" className="pb-8 mx-auto" />
-    <Input
-      type="text"
-      label="Access Token"
-      placeholder="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-      className="max-w-full"
-    />
-    <Link className="block mt-2 text-sm text-blue-500" href="#">
-      Request an access token
-    </Link>
-    <Button className="w-full mt-8" color="primary">
-      Start my registration
-    </Button>
-  </div>
-</main>
-
-    );
+  return (
+    <main className="flex items-center justify-center min-h-screen px-4">
+      <div className="w-full max-w-md p-8 h-fit bg-white rounded-md shadow-md">
+        <Logo alt="logo vlinder" className="pb-8 mx-auto" />
+        <Input
+          type="text"
+          label="Access Token"
+          placeholder="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+          className="max-w-full"
+        />
+        <Link className="block mt-2 text-sm text-blue-500" href="./register/request-access" 	replace>
+          Request an access token
+        </Link>
+        <Button className="w-full mt-8" color="primary">
+          Start my registration
+        </Button>
+      </div>
+    </main>
+  );
 }
 
-export default Page
+export default Page;
