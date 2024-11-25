c656704ab3d7e1114cc1217e2f3cc9eee12facd7
Cédric <cedric.josse@student.kuleuven.be>
2024-11-25 21:59:56 +0100

    lots of filter improvement, small bug in lover filter

--- vlinder/src/app/homepage/page.tsx
+++ vlinder/src/app/homepage/page.tsx
@@ -3,33 +3,55 @@
 import React from 'react';
 import { createClient } from '@/utils/supabase/client'
 import { useState, useEffect, useCallback } from 'react';
-import { Button, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Switch, Spacer, useDisclosure } from '@nextui-org/react';
+import { Button, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Switch, Spacer, useDisclosure, Slider, CheckboxGroup, Checkbox } from '@nextui-org/react';
 
 interface Profile{
     id: string;
     username: string;
     avatar_url: string,
+    sexual_orientation: string;
+    sex_positive: boolean;
+    gender: string;
+    display_disability: boolean;
+    disability: string[];
 }
 
 
 export default function HomePage() {
     const supabase = createClient()
+    const userId = '637465ac-0729-442c-8dc8-441d2303f560';
+    const [profile, setProfile] = useState<Profile | null>(null);
     const [profiles, setProfiles] =  useState<Profile[]>([]);
+    const [loverFilter, setLoverFilter] = useState(false);
     const [smokerFilter, setSmokerFilter] = useState(false);
     const [assistanceFilter, setAssistanceFilter] = useState(false);
+    const [ageFilterValue, setAgeFilterValue] = useState<[number, number]>([18, 100]);
     const [loading, setLoading] = useState(true); 
-    const { isOpen, onOpen, onOpenChange } = useDisclosure();
-    const [isMounted, setIsMounted] = useState(false);
+    const {isOpen, onOpen, onOpenChange } = useDisclosure();
+    const [genderFilter, setGenderFilter] = useState<string[]>(["Male", "Female", "Other"]);
 
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
+    function calculateAge(birthday: string) {
+        const birthDate = new Date(birthday);
+        const age = new Date().getFullYear() - birthDate.getFullYear();
+        const m = new Date().getMonth() - birthDate.getMonth();
+        return m < 0 || (m === 0 && new Date().getDate() < birthDate.getDate()) ? age - 1 : age;
+    }
+
+    const fetchProfile = async () => {
+        const { data, error } = await supabase
+            .from('profiles')
+            .select('*')
+            .eq('id', userId)
+            .single();
+
+        setProfile(data || null);
+    };
 
     async function fetchProfiles(){
         setLoading(true);
-        let query =  supabase
+        let query = supabase
             .from('profiles')
-            .select('id, username, avatar_url');    
+            .select('id, username, avatar_url, birthday, sexual_orientation, sex_positive, gender, display_disability, disability');    
 
         if (smokerFilter == true) {
             query = query.eq('smoker', false);
@@ -38,23 +60,55 @@ export default function HomePage() {
             query = query.eq('need_assistance', false);
         }
 
+        if (loverFilter) {
+            query = query.eq('sex_positive', true);
+
+            if (profile?.sexual_orientation === 'heterosexual') {
+            query = query.neq('gender', profile.gender);
+            setGenderFilter(genderFilter.filter(gender => gender !== profile.gender));
+            } else if (profile?.sexual_orientation === 'asexual') {
+            query = query.eq('gender', profile.gender);
+            setGenderFilter([profile.gender]);
+            }
+        } else {
+            if (!genderFilter.includes("Male")) {
+                query = query.neq('gender', 'Male');
+            }
+            if (!genderFilter.includes("Female")) {
+                query = query.neq('gender', 'Female');
+            }
+            if (!genderFilter.includes("Other")) {
+                query = query.neq('gender', 'Other');
+            }
+        }
+
         const { data: profiles } = await query;
-        setProfiles(profiles || []);
+        const filteredProfiles = profiles?.filter(profile => {
+            const age = calculateAge(profile.birthday);
+            return age >= ageFilterValue[0] && age <= ageFilterValue[1];
+        });
+
+        setProfiles(filteredProfiles || []);
         setLoading(false);
     }
 
     // Fetch profiles when the page is rendered
     useEffect(() => {
-        fetchProfiles();
+        const fetchData = async () => {
+            await fetchProfile();
+            fetchProfiles();
+        };
+        fetchData();
     }, []);
 
-    const handleApplyFilters = () => {
-        fetchProfiles(); // Re-fetch profiles when filters are applied
-      };
+    // Apply filters when user is looking for a lover
+    useEffect(() => {
+        if (!loverFilter) {
+            setGenderFilter(["Male", "Female", "Other"]);
+        }
+        fetchProfiles();
+    }, [loverFilter]);
 
-    if (!isMounted) {
-        return null; // Render nothing on the server because otherwise it will throw a hydration error
-    }
       
     return (
         <div style={{ padding: '2rem', overflowY: 'auto', maxHeight: '100vh' }}>
@@ -75,6 +129,7 @@ export default function HomePage() {
                                 <h3>Filters</h3>
                             </ModalHeader>
                                 <ModalBody>
+                                    <Spacer y={1} />
                                     <div>
                                         <h4>Not A Smoker</h4>
                                         <Switch 
@@ -92,12 +147,42 @@ export default function HomePage() {
                                             onValueChange={setAssistanceFilter} 
                                         />
                                     </div>
+                                    <Spacer y={1} />
+                                    <div>
+                                    <CheckboxGroup
+                                        label="Gender"
+                                        orientation="horizontal"
+                                        value = {genderFilter}
+                                        onValueChange={setGenderFilter}
+                                        isReadOnly={loverFilter}
+                                    >
+                                        <Checkbox value="Male">Male</Checkbox>
+                                        <Checkbox value="Female">Female</Checkbox>
+                                        <Checkbox value="Other">Other</Checkbox>
+                                    </CheckboxGroup>
+                                    </div>
+                                    <Spacer y={1} />
+                                    <div>
+                                    <Slider
+                                        data-testid="age-slider"
+                                        label="Age"
+                                        value={[ageFilterValue[0], ageFilterValue[1]]}
+                                        onChange={(value) => {
+                                            if (Array.isArray(value)) {
+                                                setAgeFilterValue([value[0], value[1]]);
+                                            }
+                                        }}
+                                        minValue={18}
+                                        maxValue={100}
+                                        step={1}
+                                        />
+                                    </div>
                                 </ModalBody>
                             <ModalFooter>
                                 <Button color="danger" variant="light" onPress={onClose}>
                                     Close
                                 </Button>
-                                <Button color="success" onPress={() => { handleApplyFilters(); onClose(); }}>
+                                <Button color="success" onPress={() => { fetchProfiles(); onClose(); }}>
                                     Apply Filters
                                 </Button>
                                 // TODO: Need help understanding filters button to have it in a more question like format
@@ -112,16 +197,28 @@ export default function HomePage() {
                 {loading ? (
                 <p>Loading...</p> // Display loading state while fetching profiles
                 ) : (
-                <ul>
-                    {profiles.map(profile => (
-                        <div key={profile.id}>
-                            <h2>
-                                <a href={`/profile/${profile.id}`}>{profile.username}</a>
-                            </h2>
-                            <img src={profile.avatar_url} alt={`${profile.username}'s avatar`} />
-                        </div>
-                    ))}
-                </ul> 
+                    <>
+                        {profile?.sex_positive && (
+                            <div>
+                                <h4>Looking for a lover?</h4>
+                                <Switch 
+                                    data-testid="lover-switch"
+                                    isSelected={loverFilter} 
+                                    onValueChange={setLoverFilter} 
+                                />
+                            </div>
+                        )}
+                        <ul>
+                            {profiles.map(profile => (
+                                <div key={profile.id}>
+                                    <h2>
+                                        <a href={`/profile/${profile.id}`}>{profile.username}</a>
+                                    </h2>
+                                    <img src={profile.avatar_url} alt={`${profile.username}'s avatar`} />
+                                </div>
+                            ))}
+                        </ul>
+                    </>
                 )}
             </div>
         </div>
