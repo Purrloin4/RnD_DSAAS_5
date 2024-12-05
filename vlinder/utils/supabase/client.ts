0f2b56146617b7132f515ced7937c5e47aa2a85b
Eltaj Amirli <amirli.eltaj18@gmail.com>
2024-12-05 15:01:44 +0100

    SOLVED MERGE CONFLICTS I THINK

--- vlinder/utils/supabase/client.ts
+++ vlinder/utils/supabase/client.ts
@@ -2,8 +2,9 @@ import { createBrowserClient } from '@supabase/ssr'
 import {Database} from "@/types/supabase"
 
 
+
+const schema = process.env.NODE_ENV === 'test' ? 'testing' : 'public';
 export function createClient() {
-  const schema = process.env.NODE_ENV === 'test' ? 'testing' : 'public';
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
