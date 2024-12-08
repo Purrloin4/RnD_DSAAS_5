25cd7923362d3fa2d25a6d15433b172c8174fb72
Cédric <cedric.josse@student.kuleuven.be>
2024-12-08 13:56:48 +0100

    fkn hell

--- vlinder/src/app/register/[accessToken]/@step_five/page.tsx
+++ vlinder/src/app/register/[accessToken]/@step_five/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 
 //Images
@@ -10,9 +10,49 @@ import ProfilePlaceholderImage from "Images/Profile_avatar_placeholder.png";
 import { Chip } from "@nextui-org/react";
 import { Textarea } from "@nextui-org/react";
 
-//Icons
+// Backend
+import { useRouter, usePathname } from "next/navigation";
+import { createClient } from "@/utils/supabase/client";
+const supabase = createClient();
+
+interface Hobby {
+  id: number;
+  name: string;
+  emoji: string;
+}
+
+interface ProfileHobby {
+  hobbies: Hobby;
+}
+
+interface Profile {
+  id: string;
+  username: string;
+  avatar_url: string;
+  sexual_orientation: string;
+  sex_positive: boolean;
+  gender: string;
+  display_disability: boolean;
+  disability: string;
+  profile_hobbies: ProfileHobby[];
+}
 
 export default function Page() {
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
+  const router = useRouter();
+  const pathName = usePathname();
+
+  const [userId, setUserId] = useState<string | null>(null);
+  const [profile, setProfile] = useState<Profile | null>(null);
+
+  function calculateAge(birthday: string) {
+    const birthDate = new Date(birthday);
+    const age = new Date().getFullYear() - birthDate.getFullYear();
+    const m = new Date().getMonth() - birthDate.getMonth();
+    return m < 0 || (m === 0 && new Date().getDate() < birthDate.getDate()) ? age - 1 : age;
+  }
+
   const fullName = "John Doe";
   const age = 21;
   const city = "Leuven";
@@ -38,6 +78,57 @@ export default function Page() {
     },
   ];
   ///
+
+  const fetchUser = async () => {
+    const accessToken = pathName.split("/").pop();
+
+    const { data: tokenData, error: tokenError } = await supabase
+    .from("accessToken")
+    .select("*")
+    .eq("id", accessToken)
+    .eq("is_used", false)
+    .single();
+
+
+    if (tokenError || !tokenData) {
+      router.push(`/register`);
+      return;
+    }
+
+    const { data, error } = await supabase.auth.getUser();
+    if (error || !data) {
+      setMessage("PLease first enter your credentials");
+      router.push(`/register/${accessToken}`);
+      return;
+    } else {
+        setUserId(data.user.id);
+        }
+    };
+    fetchUser();
+
+  const fetchProfile = async () => {
+    const { data: profileData, error: profileError } = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("id", userId)
+      .single();
+
+    setProfile(profileData || null);
+    console.log("profileData", profileData);
+
+  };
+
+  const handleStepFiveRegistration = async () => {
+    console.log("profile", profile);
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+        await fetchProfile();
+        handleStepFiveRegistration();
+    };
+    fetchData();
+}, [userId]);
 
   const [selectedHobbysItems, setSelectedHobbysItems] = useState<number[]>([]);
   const toggleHobby = (id: number) => {
